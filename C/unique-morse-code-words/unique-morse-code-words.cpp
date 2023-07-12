96a88d7cd48e92504b7c5c77720d0aeb71133267
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2023-07-12 22:12:22 +0530

    Time: 4 ms (72.52%) | Memory: 8.8 MB (16.70%) - LeetSync

--- C/unique-morse-code-words/unique-morse-code-words.cpp
+++ C/unique-morse-code-words/unique-morse-code-words.cpp
@@ -5,23 +5,17 @@ public:
         vector<string>unique;
         for(int i=0;i<words.size();i++){
             string temp="";
-            for(int j=0;words[i][j]!='\0';j++){
+            for(int j=0;words[i][j]!='\0';j++)
                 temp+=v[words[i][j]-97];
-            }
             unique.push_back(temp);
         }
-        for(int j=0;j<unique.size();j++){
-                cout<<unique[j]<<" ";
-            }
         int sum=unique.size();
-        for(int i=0;i<unique.size();i++){
-            for(int j=i+1;j<unique.size();j++){
+        for(int i=0;i<unique.size();i++)
+            for(int j=i+1;j<unique.size();j++)
                 if(unique[i]==unique[j]){
                     sum--;
                     break;
                 }
-            }
-        }
         return sum;
     }
 };
