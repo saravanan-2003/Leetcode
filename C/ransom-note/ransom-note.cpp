0d17f094e5bd0c76f3ecb1e33663676b35dce271
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2023-07-14 23:15:36 +0530

    Time: 12 ms (84.52%) | Memory: 8.7 MB (63.42%) - LeetSync

---
+++ C/ransom-note/ransom-note.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq_a[26];
+        int freq_b[26];
+        for(int i=0;i<26;i++){
+            freq_a[i]=0;
+            freq_b[i]=0;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            freq_a[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.length();i++){
+            freq_b[magazine[i]-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq_b[i]==0 && freq_a[i]!=0)
+                return 0;
+            else if(freq_a[i]>freq_b[i])
+                return 0;
+        }
+        return 1;
+    }
+};
