84b52b1d06552b1305c37c019da022703a019ac4
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2023-07-15 22:41:49 +0530

    Time: 3 ms (44.26%) | Memory: 6.2 MB (56.39%) - LeetSync

--- C/2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.cpp
+++ C/2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     bool equalFrequency(string word) {
         int freq[26];
-        for(int i=0;i<26;i++){
+        for(int i=0;i<26;i++)
             freq[i]=0;
-        }
-        for(int i=0;i<word.length();i++){
+        for(int i=0;i<word.length();i++)
             freq[word[i]-'a']++;
-        }
         for(int i=0;i<26;i++){
             for(int j=i+1;j<26;j++){
                 if(freq[i]<freq[j]){
@@ -19,52 +17,41 @@ public:
         }
         int index=26;
         for(int j=25;j>=0;j--){
-
                 if(freq[j]!=0){
                     index=j+1;
                     break;
-
-                }//cout<<freq[j]<<" \t";
-            }
-    int count=0;
-        for(int i=0;i<index;i++){
-            if(freq[i]==1){
+                }
+        }
+        int count=0;
+        for(int i=0;i<index;i++)
+            if(freq[i]==1)
                 count++;
-            }
-        }
-        
-        if(count==index && count!=0){
-            cout<<"all are 1\n";
-            return 1;
-        }
 
+        if(count==index && count!=0)
+            return 1;
+        
         //chance 1
          count=0;
-        for(int i=0;i<index-1;i++){
-            if(freq[i]==freq[i+1]){
+        for(int i=0;i<index-1;i++)
+            if(freq[i]==freq[i+1])
                 count++;
-            }
-        }
+
         //chance 2
-        if(count==index && count!=0){
-            cout<<"all are   0 1\n ";
+        if(count==index && count!=0)
             return 0;
-        }
+        
         //chance 3
         else{
             int dcount=0,thatindex;
                 for(int i=0;i<index;i++){
                     if(freq[i]==1){
                     dcount++;
-                    //thatindex=i
-                //cout<<count<<"   ";
                     }
                 }
-            if(dcount==1){
+            if(dcount==1)
                 if(count+dcount+1==index)
                     return 1;
-                
-            }
+            
             if(index==1){
                 return 1;
             }
