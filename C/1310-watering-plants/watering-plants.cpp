5148ccdf8a218ec1aafae0f19d84ecc063150592
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2023-07-17 22:54:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (98.08%) - LeetSync

--- C/1310-watering-plants/watering-plants.cpp
+++ C/1310-watering-plants/watering-plants.cpp
@@ -4,8 +4,8 @@ public:
         int steps=0,cap=capacity;
         for(int i=0;i<plants.size();i++){
             if(plants[i]<=capacity){
-                capacity=capacity-plants[i];
-                steps++;
+                capacity-=plants[i];
+                steps+=1;
             }
             else{
                 steps+=i+i+1;
