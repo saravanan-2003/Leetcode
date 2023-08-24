2afd04352cebccddfc996efe3d2b0f4c780129f6
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2023-08-24 23:57:39 +0530

    Time: 11 ms (8.97%) | Memory: 10.5 MB (5.98%) - LeetSync

--- C/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ C/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -5,11 +5,15 @@ public:
         for(int i=0;i<s.length();i++){
             // cout<<s.substr(i,part.length())<<"\t"<<i<<"\n";
             if(s.substr(i,part.length())==part){
+                //idea 1
                 // temp_left=s.substr(0,i);
                 // temp_right=s.substr(i+part.length(),s.length());
                 // temp=temp_left+temp_right;
                 // s=temp;
-                // s=s.substr(0,i)+s.substr(i+part.length(),s.length());
+                
+                //idea 2 step size reduce
+                // s=s.substr(0,i)+s.substr(i+part.length(),s.length());        
+                //idea 3 rewrite idea 2
                 s=s.erase(i,part.length());
                 i=-1;
             }
