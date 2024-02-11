1f8ef3d2a55c03d32e41d842ea146f63cf224dbb
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-11 23:39:25 +0530

    Time: 8 ms (21.68%) | Memory: 12.7 MB (11.05%) - LeetSync

---
+++ C/443-string-compression/string-compression.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        // sort(chars.begin(),chars.end())
+        vector<char>c;
+        int count=1,i;
+        for(i=0;i<chars.size()-1;i++){
+            
+            if(chars[i]==chars[i+1]){
+                count++;
+            }
+            else{
+                c.push_back(chars[i]);
+                if(count==1){
+
+                }
+                else{
+                    if(count<=9){
+                        char std=count+'0';
+                        c.push_back(std);
+                    }
+                    else{
+                        int change=count;
+                        vector<int>v;
+                        while(change){
+                            v.push_back(change%10);
+                            change=change/10;
+                        }
+                        reverse(v.begin(),v.end());
+                        for(int i=0;i<v.size();i++){
+                            char std=v[i]+'0';
+                            c.push_back(std);
+                        }
+                    }
+                }
+                count=1;
+            }
+        }
+        int change=count;
+        c.push_back(chars[i]);
+        if(count==1){
+
+        }
+        else{
+            vector<int>v;
+            while(change){
+                v.push_back(change%10);
+                change=change/10;
+            }
+            reverse(v.begin(),v.end());
+            for(int i=0;i<v.size();i++){
+                char std=v[i]+'0';
+                c.push_back(std);
+            }
+        }
+                    
+        chars.clear();
+        for(int i=0;i<c.size();i++){
+            chars.push_back(c[i]);
+        }
+        return chars.size();
+    }
+};
