6dfc56ec798f7560dcb677102296af6e183088c5
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-12 15:35:00 +0530

    Time: 10 ms (87.57%) | Memory: 22.1 MB (18.20%) - LeetSync

---
+++ C/169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        for(auto i:m){
+            if(i.second>nums.size()/2){
+                return i.first;
+            }
+            cout<<i.first<<" "<<i.second<<endl;
+        }
+        return 0;
+    }
+};
