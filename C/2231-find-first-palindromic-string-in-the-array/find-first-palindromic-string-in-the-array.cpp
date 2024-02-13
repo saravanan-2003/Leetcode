1c72e466595e5833a3286d3834597011e2399f06
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-13 21:29:04 +0530

    Time: 34 ms (97.69%) | Memory: 23.4 MB (61.43%) - LeetSync

--- C/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ C/2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        for(int i=0;i<words.size();i++){
+        int start,flag,end,i;
+        for( i=0;i<words.size();i++){
         //cout<<words[i]<<" "<<words[i].size();
-        int end=words[i].size()-1;
-        int start=0;
-        int flag=0;
-        while(start<end){
-            if(words[i][start]!=words[i][end]){
-                cout<<"not equal\n";
-                flag=1;
-                break;
+         end=words[i].size()-1;
+         start=0;
+         flag=0;
+            while(start<end){
+                if(words[i][start]!=words[i][end]){
+                    flag=1;
+                    break;
+                }
+                else{
+                    start++;
+                    end--;
+                }
             }
-            else{
-                start++;
-                end--;
+            if(flag==0){
+                return words[i];
             }
         }
-        if(flag==0){
-            // cout<<"palindrome string\n";
-            return words[i];
-            //break;
-        }
-    }
     return "";
     }
 };
