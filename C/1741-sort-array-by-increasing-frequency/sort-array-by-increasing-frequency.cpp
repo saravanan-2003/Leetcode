ec333b063ad6f3247974de6b34a9fe91b7e273ae
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-13 23:11:57 +0530

    Time: 9 ms (35.42%) | Memory: 14.4 MB (40.82%) - LeetSync

---
+++ C/1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int,int>m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        vector<vector<int>>v1;
+        for(auto i:m){
+            v1.push_back({i.second,i.first});
+        }
+        // cout<<"count   number"<<endl;
+        // for(int i=0;i<v1.size();i++){
+        //     cout<<v1[i][0]<<" "<<v1[i][1]<<endl;
+        // }
+        
+
+        int i, j;
+    bool swapped;
+    for (i = 0; i < v1.size() - 1; i++) {
+        swapped = false;
+        for (j = 0; j < v1.size() - i - 1; j++) {
+            if(v1[j][0]==v1[j+1][0]){
+                if(v1[j][1]<v1[j+1][1]){
+                    //swap(v1[j][0], v1[j + 1][0]);
+                swap(v1[j][1], v1[j + 1][1]);
+                }
+            }
+            else if (v1[j][0] > v1[j + 1][0]) {
+                swap(v1[j][0], v1[j + 1][0]);
+                swap(v1[j][1], v1[j + 1][1]);
+                swapped = true;
+            }
+        }
+        // if (swapped == false)
+        //     break;
+    }
+
+cout<<"count   number"<<endl;
+        for(int i=0;i<v1.size();i++){
+            cout<<v1[i][0]<<" "<<v1[i][1]<<endl;
+        }
+        vector<int>v;
+        for(int i=0;i<v1.size();i++){
+            for(int j=0;j<v1[i][0];j++){
+                v.push_back(v1[i][1]);
+            }
+        }
+        return v;
+
+    }
+};
