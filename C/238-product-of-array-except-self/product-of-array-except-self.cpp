e5fbf7f246087a25148862fa8ba37d3d8c6b9137
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-15 22:25:44 +0530

    Time: 12 ms (89.40%) | Memory: 26.4 MB (36.05%) - LeetSync

--- C/238-product-of-array-except-self/product-of-array-except-self.cpp
+++ C/238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int>v;
+        vector<int>v(nums.size());
+        fill(v.begin(),v.end(),1);
         int mul=1;
+        // for(int i=0;i<nums.size();i++){
+        //     mul=mul*nums[i];
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         int tp=1;
+        //         for(int j=0;j<nums.size();j++){
+        //             if(i!=j){
+        //                 tp=tp*nums[j];
+        //             }
+        //         }
+        //         v.push_back(tp);
+        //     }
+        //     else if(mul==0){
+        //         v.push_back(0);
+        //     }
+        //     else{
+        //         v.push_back(mul/nums[i]);
+        //     }
+        // }
+        //prefix
         for(int i=0;i<nums.size();i++){
+            v[i]=v[i]*mul;
             mul=mul*nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                int tp=1;
-                for(int j=0;j<nums.size();j++){
-                    if(i!=j){
-                        tp=tp*nums[j];
-                    }
-                }
-                v.push_back(tp);
-            }
-            else if(mul==0){
-                v.push_back(0);
-            }
-            else{
-                v.push_back(mul/nums[i]);
-            }
+        mul=1;
+        for(int i=nums.size()-1;i>=0;i--){
+            v[i]=v[i]*mul;
+            mul=mul*nums[i];
         }
         return v;
     }
