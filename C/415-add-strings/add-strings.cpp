2bcdbe8460a30c58c71ebe3449076d3294322a81
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-19 22:24:06 +0530

    Time: 5 ms (55.26%) | Memory: 8.2 MB (49.19%) - LeetSync

--- C/415-add-strings/add-strings.cpp
+++ C/415-add-strings/add-strings.cpp
@@ -18,7 +18,39 @@ public:
             tadd_last=tadd%10;
             extra=tadd/10;
             //cout<<tadd_last+'0'<<endl;
-            temp+=to_string(tadd_last);
+            //temp+=to_string(tadd_last);
+            switch(tadd_last){
+                case 0:
+                    temp+='0';
+                    break;
+                case 1:
+                    temp+='1';
+                    break;
+                case 2:
+                    temp+='2';
+                    break;
+                case 3:
+                    temp+='3';
+                    break;
+                case 4:
+                    temp+='4';
+                    break;
+                case 5:
+                    temp+='5';
+                    break;
+                case 6:
+                    temp+='6';
+                    break;
+                case 7:
+                    temp+='7';
+                    break;
+                case 8:
+                    temp+='8';
+                    break;
+                case 9:
+                    temp+='9';
+                    break;
+            }
         }
         reverse(temp.begin(),temp.end());
         return temp;
