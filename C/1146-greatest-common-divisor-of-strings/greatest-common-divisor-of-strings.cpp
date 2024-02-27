a87a142d44b45f10b7216d9920affe9a0f5e52b9
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-02-27 15:37:30 +0530

    Time: 16 ms (6.54%) | Memory: 36.4 MB (5.75%) - LeetSync

--- C/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ C/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        int end;
-        // if(str1.size()<str2.size()){
-        //     int l=1;
-        // while(l<str2.size()){
-        //     int i=0;
-        //     int k=l;
-        //     while(i<str1.size()){
-        //         if(str1.substr(i,l)==str2.substr(0,l)){
-        //             i=i+l;
-        //             end=i;
-        //         }
-        //         else{
-        //             l++;
-        //             break;
-        //         }
-        //     }
-        //     if(end==str1.size()){
-        //         return str2.substr(0,l);
-        //         break;
-        //     }
-        // }
-            
-        // }
-        string temp=str2;
-        while(str2.size()!=0){
-            int i=0;
-            while(i<str1.size()){
-                if(str1.substr(i,str2.size())==str2){
-                    i=i+str2.size();
-                    end=i;
-                }
-                else{
-                    str2=str2.substr(0,str2.size()-1);
-                    //cout<<str2.size()<<endl;
+        int end, i, h;
+        string temp = str2;
+        while (str2.size() != 0) {
+            i = 0;
+            while (i < str1.size()) {
+                if (str1.substr(i, str2.size()) == str2) {
+                    i = i + str2.size();
+                    end = i;
+                } else {
+                    str2 = str2.substr(0, str2.size() - 1);
                     break;
                 }
             }
-            if(end==str1.size()){
-                int h=0;
-                while(temp.substr(h,str2.size())==str2){
-                    h+=str2.size();
+            if (end == str1.size()) {
+                h = 0;
+                while (temp.substr(h, str2.size()) == str2) {
+                    h += str2.size();
                 }
-                if(h==temp.size()){
+                if (h == temp.size()) {
                     break;
+                } else {
+                    str2 = str2.substr(0, str2.size() - 1);
                 }
-                else{
-                    str2=str2.substr(0,str2.size()-1);
-                }
-               
             }
         }
         return str2;
