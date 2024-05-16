0816a52d388a3d45db947753404b84f06e370ba3
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-05-16 16:17:16 +0530

    Time: 15 ms (7.35%) | Memory: 27.2 MB (73.64%) - LeetSync

---
+++ C/3226-minimum-number-game/minimum-number-game.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        vector<int>temp;
+        for(int i=0;i<nums.size()/2;i++){
+            int min=INT_MAX,min1=INT_MAX,locate1,locate2;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]<min){
+                    min=nums[i];
+                    locate1=i;
+                }
+            }
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]<min1 && i!=locate1){
+                    min1=nums[i];
+                    locate2=i;
+                }
+            }
+            
+            cout<<min<<" "<<min1<<"\n";
+            temp.push_back(min1);
+            temp.push_back(min);
+            nums[locate1]=100;
+            nums[locate2]=100;
+        }
+        return temp;
+    }
+};
