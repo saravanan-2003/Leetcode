28fed17c904d10b1b81e0a03c42bba5b6d867134
SARAVANAN T <87331965+saravanan-2003@users.noreply.github.com>
2024-05-16 15:58:51 +0530

    Time: 6 ms (35.18%) | Memory: 5.5 MB (79.20%) - LeetSync

--- C/2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.c
+++ C/2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.c
@@ -1,9 +1,9 @@
-int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target){
-     int count=0;
-    for(int i=0;i<hoursSize;i++){
-        if(hours[i]>=target){
-            count++;
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
+    int counter = 0;
+    for (int i = 0; i < hoursSize; i++) {
+        if (hours[i] >= target) {
+            counter++;
         }
     }
-    return count;
+    return counter;
 }
